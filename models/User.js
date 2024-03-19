ef621237076c80cfaac2bd15e93a2c277cfc846d
siddharthadas15 <67184958+siddharthadas15@users.noreply.github.com>
2024-03-20 03:43:59 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ var mongoose=require("mongoose");
 var passportLocalMongoose=require("passport-local-mongoose");
 var UserSchema=new mongoose.Schema({
    username:String,
-   password:String,
    userq:[{
        type: mongoose.Schema.Types.ObjectId,
        ref:"Question"
